61c5be1087c02715b1f8f2f8d13c7e517abbb3ff
Remon Lam <remon.lam@lamnet.nl>
2015-11-18 23:06:03 +0100

    updates

--- image-builder.sh
+++ image-builder.sh
@@ -3,35 +3,43 @@
 # Do some pre-requirements
 # Install wget and badtar
 yum install -y wget bsdtar
+wget -P /tmp https://raw.githubusercontent.com/remonlam/rpi-archlinux/master/configure-system.sh
+chmod 755 /tmp/configure-system.sh
 
 ### SCRIPT VARIABLES
 ## Ask user for system specific variables
 read -p 'Enter device name (SD-Card): like sdb: ' sdCard
-
 part1=1
 part2=2
-##fdisk /dev/$sdCard
 
+##fdisk /dev/$sdCard
 # Create parition layout
 # NOTE: This will create a partition layout as beeing described in the README...
 (echo o; echo n; echo p; echo 1; echo ; echo +100M; echo t; echo c; echo n; echo p; echo 2; echo ; echo ; echo w) | fdisk /dev/$sdCard
 
-# Create and mount the FAT filesystem:
-mkfs.vfat /dev/$sdCard$part1""
+# Sync disk
+sync
+
+#Create and mount the FAT filesystem:
+mkfs.vfat /dev/$sdCard$part1
 mkdir boot
 mount /dev/$sdCard$part1 boot
 
-# Create and mount the ext4 filesystem:
+#Create and mount the ext4 filesystem:
 mkfs.ext4 /dev/$sdCard$part2
 mkdir root
 mount /dev/$sdCard$part2 root
 
-# Download and extract the root filesystem (as root, not via sudo):
+#Download and extract the root filesystem (as root, not via sudo):
 wget http://archlinuxarm.org/os/ArchLinuxARM-rpi-2-latest.tar.gz
 bsdtar -xpf ArchLinuxARM-rpi-2-latest.tar.gz -C root
 sync
 
-# Move boot files to the first partition:
+#Move boot files to the first partition:
 mv root/boot/* boot
-Unmount the two partitions:
+
+# Copy "configure-system.sh" script to "root"
+mv /tmp/configure-system.sh root
+
+#Unmount the two partitions:
 umount boot root
